74d01814862e0af66929a30808e00f38400d7ae4
duongvanQuang-bit230347 <dvquang1209@gmail.com>
2025-09-06 18:06:50 +0700

    spring security va cau hinh Auth

---
+++ Spring Security, Authentication với JWT/Identity-service/Identity-service/src/main/java/com/quang/Identity_service/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.quang.Identity_service.controller;
+
+import com.quang.Identity_service.dto.request.ApiReponse;
+import com.quang.Identity_service.dto.request.UserCreationRequest;
+import com.quang.Identity_service.dto.request.UserUpdateRequest;
+import com.quang.Identity_service.dto.response.UserReponse;
+import com.quang.Identity_service.service.UserSevice;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+
+    UserSevice userSevice;
+
+    @PostMapping
+    ApiReponse<UserReponse> createUser(@RequestBody @Valid UserCreationRequest request) {
+        ApiReponse<UserReponse> apiReponse = new ApiReponse<>();
+        apiReponse.setResult(userSevice.createUser(request));
+        return apiReponse;
+    }
+
+    @GetMapping
+    List<UserReponse> getUsers() {
+        return userSevice.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    UserReponse getUser(@PathVariable("userId") String userId) {
+        return userSevice.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    UserReponse updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userSevice.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userSevice.deleteUser(userId);
+        return "user da xoa";
+    }
+}
