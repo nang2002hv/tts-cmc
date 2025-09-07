a0a38f7fc3231306b36e0e778d0331b008b80bfd
duongvanQuang-bit230347 <dvquang1209@gmail.com>
2025-09-07 15:37:45 +0700

    Role,permission với PreAuthorize

---
+++ Role,permission với PreAuthorize/Identity-service/Identity-service/src/main/java/com/quang/Identity_service/dto/request/UserUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.quang.Identity_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+     String password;
+     String firstName;
+     String lastName;
+     LocalDate dob;
+     List<String> roles;
+
+
+}
